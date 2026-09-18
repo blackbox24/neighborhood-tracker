c40a7371736facec932d98915570eb1e58873f6e
blackbox24 <nicardid@gmail.com>
2026-09-18 19:50:45 +0000

    feat: added example service worker

--- app.js
+++ app.js
@@ -13,5 +13,18 @@ function updateStatus() {
     }
 }
 
+// Check if the browser supports Service Workers
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js')
+            .then(registration => {
+                console.log('✅ Service Worker registered successfully with scope: ', registration.scope);
+            })
+            .catch(error => {
+                console.error('❌ Service Worker registration failed: ', error);
+            });
+        });
+}
+
 // Run once on initial load
 updateStatus();
